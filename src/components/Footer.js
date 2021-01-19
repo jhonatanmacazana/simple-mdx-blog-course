0f088a53fb7a5ace018b47cac1595f156d662ec9
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-01-19 15:49:51 -0500

    still developing footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import { FooterWrapper } from "../elements"
+
+export const Footer = ({ children }) => {
+  return <FooterWrapper>oa</FooterWrapper>
+}
---
+++ src/elements/FooterElements.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const FooterWrapper = styled.footer`
+  grid-column: 2 / span 12;
+  min-height: 11.25rem;
+  padding: 3rem 0;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+
+  @media ${props => props.theme.breakpoints.tablet} {
+    grid-column: 2 / 6;
+  }
+`
