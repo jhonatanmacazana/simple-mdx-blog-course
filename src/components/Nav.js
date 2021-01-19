8c9f8e7537d309405ffe24992c95fb11471e9d3c
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2021-01-19 15:40:05 -0500

    added navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import { NavWrapper } from "../elements"
+
+export const Nav = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "logo.svg" }) {
+        publicURL
+      }
+    }
+  `)
+
+  return (
+    <NavWrapper>
+      <Link to="/">
+        <img src={data.logo.publicURL} alt="logo" />
+      </Link>
+    </NavWrapper>
+  )
+}
